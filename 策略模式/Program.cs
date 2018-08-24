ff6fa51d7fe55a031c499ac6fa3c46a71c4bbade
刘朋芳 <lpf_mart@163.com>
2018-08-24 15:11:03 +0800

    简单工厂

---
+++ 策略模式/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 简单工厂
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // 现金收费抽象类
+    abstract class CashSuper
+    {
+        public abstract double acceptCash(double money);
+    }
+
+    // 正常收费子类
+    class CashNormal : CashSuper
+    {
+        public override double acceptCash(double money)
+        {
+            return money;
+        }
+    }
+
+    //打折收费子类
+    class CashRebate : CashSuper
+    {
+        private double moneyRebate = 1d;
+        public CashRebate(string moneyRebate)
+        {
+            this.moneyRebate = double.Parse(moneyRebate);
+        }
+
+        public override double acceptCash(double money)
+        {
+            return money * moneyRebate;
+        }
+    }
+
+    // 返利收费子类
+    class CashReturn : CashSuper
+    {
+        private double moneyCondition = 0.0d;
+        private double moneyReturn = 0.0d;
+        public CashReturn(string moneyCondition , string moneyReturn)
+        {
+            this.moneyCondition = double.Parse(moneyCondition);
+            this.moneyReturn = double.Parse(moneyReturn);
+        }
+
+        public override double acceptCash(double money)
+        {
+            double result = money;
+            if (money >= moneyCondition)
+            {
+                result = money - Math.Floor(money / moneyCondition) * moneyReturn;
+            }
+            return result;
+        }
+    }
+
+    // 现金收费工厂
+    class CashFactory
+    {
+        public static CashSuper createCashAccept(string type)
+        {
+            CashSuper cs = null;
+            switch (type)
+            {
+                case "正常收费":
+                    cs = new CashNormal();
+                    break;
+                case "满300返100":
+                    cs = new CashReturn("300","100");
+                    break;
+                case "打8折":
+                    cs = new CashRebate("0.8");
+                    break;
+            }
+            return cs;
+        }
+    }
+
+}
