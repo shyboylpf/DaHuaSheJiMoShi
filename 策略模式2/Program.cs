55b2aba35d35382291e013c11161c339e2d9f873
刘朋芳 <lpf_mart@163.com>
2018-08-27 10:43:25 +0800

    策略模式 , 用来封装算法

---
+++ 策略模式2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 策略模式2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // 抽象算法类
+    abstract class Strategy
+    {
+        // 算法方法
+        public abstract void AlgorithmInterface();
+    }
+
+    // 具体算法A
+    class ConcreteStrategyA : Strategy
+    {
+        // 实现算法A
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("算法A实现");
+        }
+    }
+    class Context
+    {
+        Strategy strategy;
+        public Context(Strategy strategy)  // 传入具体的策略对象
+        {
+            this.strategy = strategy;
+        }
+        // 上下文接口
+        public void ContextInterface()  // 根据具体的策略对象 , 调用其算法的方法.
+        {
+            strategy.AlgorithmInterface();
+        }
+    }
+}
