994bed187683ba43c296bd85a91876cf1ad79b5d
刘朋芳 <lpf_mart@163.com>
2018-09-17 15:23:10 +0800

    中介者模式

---
+++ 中介者模式/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 中介者模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteMediator mediator = new ConcreteMediator();
+
+            ConcreteColleague1 colleague1 = new ConcreteColleague1(mediator);
+            ConcreteColleague2 colleague2 = new ConcreteColleague2(mediator);
+
+            mediator.Colleague1 = colleague1;
+            mediator.Colleague2 = colleague2;
+
+            colleague1.Send("吃过饭了吗?");
+            colleague2.Send("没有呢 , 你打算请客吗?");
+        }
+    }
+
+    public abstract class Mediator
+    {
+        public abstract void Send(string message, Colleague colleague);
+    }
+
+    public class Colleague
+    {
+        protected Mediator mediator;
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+
+    class ConcreteMediator : Mediator
+    {
+        private ConcreteColleague1 colleague1;
+        private ConcreteColleague2 colleague2;
+        public ConcreteColleague1 Colleague1
+        {
+            set { colleague1 = value; }
+        }
+        public ConcreteColleague2 Colleague2
+        {
+            set { colleague2 = value; }
+        }
+        public override void Send(string message, Colleague colleague)
+        {
+            if(colleague == colleague1)
+            {
+                colleague2.Notify(message);
+            }
+            else
+            {
+                colleague1.Notify(message);
+            }
+        }
+    }
+    class ConcreteColleague1 : Colleague
+    {
+        public ConcreteColleague1(Mediator mediator) : base(mediator) { }
+        
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+        public void Notify(string message)
+        {
+            Console.WriteLine("同事1得到消息: " + message);
+        }
+    }
+    class ConcreteColleague2 : Colleague
+    {
+        public ConcreteColleague2(Mediator mediator) : base(mediator) { }
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+        public void Notify(string message)
+        {
+            Console.WriteLine("同事2得到消息: " + message);
+        }
+    }
+}
